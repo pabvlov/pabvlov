f24f6dfa3a609c8a19f17e6442ee3cc6bcc0ec54
pabvlov <pablojavierprietocepeda@gmail.com>
2024-11-04 19:46:56 -0300

    se quita la wea fea bugeada

--- src/app/content/quienchuchasoy/quienchuchasoy.component.ts
+++ src/app/content/quienchuchasoy/quienchuchasoy.component.ts
@@ -8,66 +8,14 @@ import { FooterComponent } from '../../shared/footer/footer.component';
   templateUrl: './quienchuchasoy.component.html',
   styleUrls: ['./quienchuchasoy.component.css']
 })
-export class AboutmeComponent implements OnInit, AfterViewInit {
+export class AboutmeComponent {
 
   @ViewChild('container') container!: ElementRef;
 
   constructor() { }
 
-  ngAfterViewInit() {
-    this.posX = this.getRandomInt(this.container.nativeElement.offsetWidth);
-    this.posY = this.getRandomInt(this.container.nativeElement.offsetHeight);
-    this.rotation = this.getRandomRotation();
-    setTimeout(() => {
-      this.newPos();
-    }, 2000);
-  }
-
   fechaNacimiento = moment([2000, 7, 7]);
   fechaActual = moment();
-  chip = 1;
-  height = 0;
-  width = 0;
+  chip = 0;
   anios = this.fechaActual.diff(this.fechaNacimiento, 'years')
-  posX: number = 0;
-  posY: number = 0;
-  rotation = this.getRandomRotation();
-  poligon = this.getRandomTriagle();
-
-  ngOnInit(): void {
-    
-
-  }
-
-  newPos() {
-    this.height = this.getRandomInt(this.container.nativeElement.offsetWidth)
-    this.width = this.getRandomInt(this.container.nativeElement.offsetHeight)
-    this.posX = this.getRandomInt(this.container.nativeElement.offsetWidth);
-    this.posY = this.getRandomInt(this.container.nativeElement.offsetHeight);
-    this.rotation = this.getRandomRotation();
-    this.poligon = this.getRandomTriagle();
-    setTimeout(() => {
-      this.newPos();
-    }, 2000);
-  }
-
-  getRandomInt(max: number) {
-    return Math.floor(Math.random() * max);
-  }
-
-  getRandomRotation() {
-      return Math.floor(Math.random() * 360);
-    
-  }
-
-  getRandomTriagle() {
-    const triangles = ['polygon(6% 4%, 86% 8%, 100% 50%, 75% 100%, 11% 68%)', 
-                      'polygon(6% 4%, 42% 84%, 95% 76%, 73% 97%, 80% 41%)', 
-                      'polygon(27% 53%, 55% 0, 79% 53%, 100% 100%, 0% 100%)',
-                      'polygon(30% 78%, 100% 26%, 0 0, 100% 100%, 0% 100%)',
-                      'polygon(100% 100%, 0 0, 100% 0, 50% 51%, 0% 100%)']
-    return triangles[Math.floor(Math.random() * triangles.length)];
-
-  }
-
 }
